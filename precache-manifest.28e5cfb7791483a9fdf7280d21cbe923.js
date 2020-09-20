7406345f7946b7220c074022d7e01c50247ec916
deveshteotia12 <devesh.teotia12@gmail.com>
2020-09-20 22:36:30 +0530

    Updates

---
+++ precache-manifest.28e5cfb7791483a9fdf7280d21cbe923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4578d436b6dee293d6970db69372cf5f",
+    "url": "/ReactbasicMonster/index.html"
+  },
+  {
+    "revision": "0a64dea515d14981f9e7",
+    "url": "/ReactbasicMonster/static/css/main.2cf353cd.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/ReactbasicMonster/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactbasicMonster/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a64dea515d14981f9e7",
+    "url": "/ReactbasicMonster/static/js/main.17f7409c.chunk.js"
+  },
+  {
+    "revision": "c565b731135dee8581b9",
+    "url": "/ReactbasicMonster/static/js/runtime-main.9082fa12.js"
+  }
+]);
